86cb08b3156eabd77751ead283b3ccf6a787bab0
sisimogangg <godfrey@sisimogang.co.za>
2019-07-09 19:05:36 +0200

    minor fixes

--- main.go
+++ main.go
@@ -2,31 +2,29 @@ package main
 
 import "fmt"
 
-//
-
-func main() {
-	clouds := [...]int{0, 1, 0, 0, 0, 1, 0}
-	//clouds := [...]int{0, 0, 1, 0, 0, 1, 0}
-	size := int32(len(clouds))
+func jumpingClouds(c [7]int32) int32 {
+	size := int32(len(c))
 	var step int32
 	stepCount := 0
 
-	//steps 2 1 1 2
-	//
 	for step < (size - 1) {
 
-		if step+2 < size && clouds[step+2] == 0 {
+		if step+2 < size && c[step+2] == 0 {
 			step = step + 2
-			fmt.Println("step : ", step)
 			stepCount++
 			continue
 		}
 
 		step++
-		fmt.Println("step : ", step)
 		stepCount++
 
 	}
+	return int32(stepCount)
+}
 
-	fmt.Println("Steps count: ", stepCount)
+func main() {
+	clouds := [...]int32{0, 1, 0, 0, 0, 1, 0}
+	//clouds := [...]int{0, 0, 1, 0, 0, 1, 0}
+
+	fmt.Println("Steps count: ", jumpingClouds(clouds))
 }
